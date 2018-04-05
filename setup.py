6579e08040babfcab5dc33b28bf4187bae4d6682
Serial Lab <slab@serial-lab.com>
2018-04-05 11:56:22 -0600

    Added requirements to setup, fixed the license header.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -10,8 +10,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=6.0',
-    # TODO: put package requirements here
+    'lxml==3.7.2',
+    'epcpyyes',
 ]
 
 test_requirements = [
@@ -23,12 +23,11 @@ setup(
     version='0.1.0',
     description="Python lxml parsing for EPCIS Events.",
     long_description=readme + '\n\n' + history,
-    author="Serial Lab, LLC",
+    author="Rob Magee",
     author_email='slab@serial-lab.com',
+    maintainer="SerialLab Corp",
     url='https://gitlab.com/serial-lab/eparsecis',
-    packages=[
-        'eparsecis',
-    ],
+    packages=find_packages(),
     package_dir={'eparsecis':
                  'eparsecis'},
     entry_points={
@@ -48,6 +47,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
