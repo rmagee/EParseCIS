eab5808a7b072a417bffcce3d1ba491dfa5373a0
Serial Lab <slab@serial-lab.com>
2018-11-07 14:27:13 -0700

    Bump version: 2.0.2 → 2.0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = []
 
 setup(
     name='eparsecis',
-    version='2.0.2',
+    version='2.0.3',
     description="Python lxml parsing for EPCIS Events.",
     long_description=readme,
     author="Rob Magee",
